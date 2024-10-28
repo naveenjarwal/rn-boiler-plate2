58ce18c79eae3b025dade982c9be166bdbdcedee
naveenjarwal <nvnjarwal@gmail.com>
2024-10-28 17:07:59 +0530

    Initial commit of custom React Native template

--- template.config.js
+++ template.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    placeholderName: "ProjectName", // Placeholder to replace in files
+    placeholderName: "MyTemplateProject2", // Placeholder to replace in files
     templateDir: "./template", // Directory containing template files
     postInitScript: "./postInstall.js" // Optional script to run after initialization
   };
